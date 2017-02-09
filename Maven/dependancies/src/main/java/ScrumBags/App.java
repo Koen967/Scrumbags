7ca2564aac8dbf5816e2d3cdd34291286fcacf54
MetalXLemmy <JeroenBierens@gmail.com>
2017-02-09 14:37:24 +0100

    project voor dependancies toegevoegd

---
+++ Maven/dependancies/src/main/java/ScrumBags/App.java
@@ -0,0 +1,13 @@
+package ScrumBags;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
