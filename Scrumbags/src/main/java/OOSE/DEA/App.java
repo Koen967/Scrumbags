2c3df91580bc54af14d96b1cf175dda055469dbd
koen <koen967@hotmail.com>
2017-02-24 10:13:59 +0100

    Een normaal Hello World project

---
+++ Scrumbags/src/main/java/OOSE/DEA/App.java
@@ -0,0 +1,13 @@
+package OOSE.DEA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
